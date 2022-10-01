4edb8f732fcd3447927f7b60a9889032320b0394
Jonoym <jonoym@github.com>
2022-10-02 01:28:56 +1000

    Finished account page

--- src/pages/Account/Account.tsx
+++ src/pages/Account/Account.tsx
@@ -1,33 +1,139 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Account.scss";
 import Navbar from '../../components/layout/Navbar/Navbar';
+import { FiUser, FiLogOut, FiClipboard, FiShield, FiEdit2, FiChevronLeft, FiMail, FiCreditCard, FiRefreshCcw } from 'react-icons/fi'
 
 const Account = () => {
 
+  const [page, setPage] = useState("default");
+
+  const getAccountPage = () => {
+    return (
+      <div className={`account-details ${page === "account" ? "" : "account-page-inactive"}`}>
+        <div className="header">
+          <div
+            className="back-button"
+            onClick={() => setPage("default")}
+          >
+              <FiChevronLeft className="back-icon"/>
+              Back
+          </div>
+          <h2 className="section">Account Details</h2>
+        </div>
+        <div className="account-options">
+          <div className="account-page-option">
+            <div className="option-icon-container">
+              <FiCreditCard className="account-page-option-icon"/>
+            </div>
+            <div className="vertical-divider"/>
+            <div className="option-container">
+              <div className="option-label">
+                Full Name
+              </div>
+              <div className="option-value">
+                Ella Smith
+              </div>
+            </div>
+          </div>
+          <div className="account-page-option">
+            <div className="option-icon-container">
+              <FiUser className="account-page-option-icon"/>
+            </div>
+            <div className="vertical-divider"/>
+            <div className="option-container">
+              <div className="option-label">
+                Username
+              </div>
+              <div className="option-value">
+                @hiking.ella
+              </div>
+            </div>
+          </div>
+          <div className="account-page-option">
+            <div className="option-icon-container">
+              <FiMail className="account-page-option-icon"/>
+            </div>
+            <div className="vertical-divider"/>
+            <div className="option-container">
+              <div className="option-label">
+                Email
+              </div>
+              <div className="option-value">
+                ellasmith@gmail.com
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="update-account-details">
+          <FiRefreshCcw className="logout-icon"/>
+          Update
+        </div>
+      </div>
+    )
+  }
+
+  const getDefaultPage = () => {
+    return (
+      <div className={`account ${page === "default" ? "" : "default-page-inactive"}`}>
+        <div className="profile-image section">
+          <div className="edit-image-container">
+            <FiEdit2 className="edit-image-icon"/>
+          </div>
+        </div>
+        <h2 className="section">Ella Smith</h2>
+        <h6 className="section username">@hiking.ella</h6>
+        <div className="section delay-1 user-stats">
+          <div className="stats-container">
+            <div className="stats-number">54</div>
+            <div className="stats-label">hikes</div>
+          </div>
+          <div className="stats-container">
+            <div className="stats-number">100</div>
+            <div className="stats-label">memos</div>
+          </div>
+          <div className="stats-container">
+            <div className="stats-number">26</div>
+            <div className="stats-label">photos</div>
+          </div>
+        </div>
+        <div className="section delay-2 account-options">
+          <div className="account-option" onClick={() => setPage("account")}>
+            <FiUser className="account-option-icon" onClick={() => setPage("account")}/>
+            <h5>
+              Account Details
+            </h5>
+          </div>
+          <div className="account-option">
+            <FiShield className="account-option-icon"/>
+            <h5>
+              Privacy &amp; Accessibility
+            </h5>
+          </div>
+          <div className="account-option">
+            <FiClipboard className="account-option-icon"/>
+            <h5>
+              Terms &amp; Conditions 
+            </h5>
+          </div>
+          <a href="/login" className="logout">
+            <FiLogOut className="logout-icon"/>
+            Log Out
+          </a>
+        </div>
+      </div>
+    )
+  }
 
   return (
-    <div className="account">
-      <div className="profile-image section"></div>
-      <h2 className="section">Ella Smith</h2>
-      <div className="account-options">
-        <div className="account-option">
-          Privacy &amp; Accessibility
-        </div>
-        <div className="divider" />
-        <div className="account-option">
-          Terms &amp; Conditions 
-        </div>
-        <div className="divider" />
-        <div className="account-option">
-          Help &amp; Documentation 
-        </div>
-        <div className="divider" />
-        <a href="/login" className="logout">
-          Log Out
-        </a>
-      </div>
-      <Navbar />
-    </div>
+    <>    
+     <Navbar />
+      {
+        getDefaultPage()
+      }
+      {
+        getAccountPage()
+      }
+    </>
   );
 
 };
