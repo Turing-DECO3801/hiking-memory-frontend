7bfb336a42e42f0f7e926658bd95aef02c9125c3
Jonoym <jonoym@github.com>
2022-10-01 12:49:15 +1000

    Added exit button to light box

--- src/pages/Photos/Lightbox.tsx
+++ src/pages/Photos/Lightbox.tsx
@@ -6,18 +6,21 @@ import "swiper/css/navigation";
 import { Navigation } from "swiper";
 import GalleryImage from './GalleryImage';
 import { Image } from "react-grid-gallery";
+import { FiX } from 'react-icons/fi';
 
 interface LightBoxProps {
   removeGallery: () => void;
   images: Image[]
+  className: string,
 }
 
-const LightBox = ({ removeGallery, images }: LightBoxProps) => {
+const LightBox = ({ removeGallery, images, className }: LightBoxProps) => {
 
-  
-  console.log(images);
-  return (
-    <div className="light-box">
+    return (
+    <div className={`light-box ${className}`}>
+      <div className="light-box-exit">
+        <FiX className="exit-icon"/>
+      </div>
       <div className="gallery">
         <Swiper navigation={true} modules={[Navigation]} loop={true} className="mySwiper">
           {
