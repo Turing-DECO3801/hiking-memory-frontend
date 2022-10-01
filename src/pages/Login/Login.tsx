85cb9090cbebbc36d85c2af2f4eb64d03584370d
Jonoym <jonoym@github.com>
2022-10-02 00:24:51 +1000

    Sign up page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -5,13 +5,17 @@ import Logo from '../../components/common/Logo/Logo';
 import Button from '../../components/common/Button/Button';
 import "./Login.scss"
 import Loading from '../../components/common/Loading/Loading';
+import { useNavigate } from 'react-router-dom';
 
 
 const Login = () => {
   const { login } = useContext(AuthContext);
 
+  const navigate = useNavigate();
+
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
   
   const onUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setUsername(event.currentTarget.value);
@@ -21,14 +25,29 @@ const Login = () => {
     setPassword(event.currentTarget.value);
   }
 
+  const showError = () => {
+    if (error) {
+      return (
+      <div className="error-message">
+        Oops! Incorrect username or password
+      </div>
+      );
+    }
+  }
+
   return (
     <>
     <div className="login">
       <div className="section login-logo">
-        <Logo />
-        <h1>hiking memory maker</h1>
+        <div className="logo-container">
+          <Logo />
+        </div>
+        <h1 className="title">memory trail</h1>
       </div>
       <br/>
+      {
+        showError()
+      }
       <div className="section delay-1">
         <TextInput placeholder="Username" icon="user" onChange={onUsernameChange}/>
         <br/>
@@ -40,13 +59,12 @@ const Login = () => {
       <div className="section delay-3">
         Don&apos;t have an account?
       </div>
-      <a className="link section delay-3">
+      <a className="link section delay-3" onClick={() => navigate("/signup")}>
         Sign up here
       </a>
       <br/>
       <br/>
       <br/>
-      <Loading />
     </div>
     </>
   );
--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,41 +1,81 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AuthContext } from '../../contexts/AuthContext';
 import TextInput from '../../components/common/TextInput/TextInput';
 import Logo from '../../components/common/Logo/Logo';
 import Button from '../../components/common/Button/Button';
 import "./SignUp.scss"
 import Loading from '../../components/common/Loading/Loading';
-
+import { useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
   const { login } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const onNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.currentTarget.value);
+  }
+
+  const onUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.currentTarget.value);
+  }
+
+  const onEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.currentTarget.value);
+  }
+
+  const onPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.currentTarget.value);
+  }
   
   return (
     <>
     <div className="login">
-      <div className="section">
-        <Logo />
-        <h1>hiking memory maker</h1>
+      <div className="section sign-up-header">
+        <div className="mini-logo-container">
+          <Logo />
+        </div>
+        <h1 className="title">memory trail</h1>
       </div>
-      <br/>
-      <div className="section delay-1">
-        <TextInput placeholder="Username" icon="user"/>
-        <br/>
-        <TextInput placeholder="Password" icon="lock" type="password"/>
-        <br/>
+      <div className="section delay-1 sign-up-container">
+        <div className="input-container">
+          <div className="input-label">
+            Name:
+          </div>
+          <input className="user-input" onChange={onNameChange}/>
+        </div>
+        <div className="input-container">
+          <div className="input-label">
+            Username:
+          </div>
+          <input className="user-input" onChange={onUsernameChange}/>
+        </div>
+        <div className="input-container">
+          <div className="input-label">
+            Email:
+          </div>
+          <input className="user-input" onChange={onEmailChange}/>
+        </div>
+        <div className="input-container">
+          <div className="input-label">
+            Password:
+          </div>
+          <input type="password" className="user-input" onChange={onPasswordChange}/>
+        </div>
       </div>
-      <Button className="section delay-2" onClick={login}>Log in</Button>
+      <Button className="section delay-2" onClick={login}>Sign Up</Button>
       <br/>
       <div className="section delay-3">
-        Don&apos;t have an account?
+        Already have an account?
       </div>
-      <a className="link section delay-3">
-        Sign up here
-      </a>
-      <br/>
-      <br/>
-      <br/>
-      <Loading />
+      <div className="link section delay-3" onClick={() => navigate("/login")}>
+        Log in here
+      </div>
     </div>
     </>
   );
