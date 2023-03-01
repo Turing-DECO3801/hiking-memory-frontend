9d9074b136175ca0af9c489bc4dcd410c927b0c1
Jonoym <86467852+Jonoym@users.noreply.github.com>
2023-03-01 22:44:58 +1000

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
-export const serverURL = "https://13.54.55.236.nip.io/";
+export const serverURL = "";
 
 export const transcriptURL = "https://api.assemblyai.com/v2";
 
-export const API_KEY = "bbb21a36f52e4d1b96ede3bf9f41f963";
+export const API_KEY = "";
